a0a273bf085c475a3527eaaad76722227c21651e
Arad <aradontheline@gmail.com>
2018-12-28 22:06:53 +0330

    10

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {SearchComponent} from './search/search.component';
-import {ProfileComponent} from './profile/profile.component';
+import { SearchComponent} from './search/search.component';
+import { ProfileComponent} from './profile/profile.component';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
